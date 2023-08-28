1f8a3eaddd8205c8d32b1c75dc77ec257f6d9ed2
Timothy-Pulliam <timothy.dpulliam@gmail.com>
2023-08-28 02:43:34 +0000

    open file in RW mode instead of WO

--- touch.c
+++ touch.c
@@ -7,7 +7,21 @@
 #include <utime.h>
 
 bool touch(const char *filename) {
-  int fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+  int fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+  // O_CREAT: This flag is used to create the file if it doesn't exist. If the
+  // file already exists, this flag has no effect.
+
+  // O_RDWR: This flag indicates that the file should be opened in read-write
+  // mode. It means that you can both read from and write to the file.
+
+  // O_WRONLY: This flag indicates that the file should be opened in write-only
+  // mode. It means that you can only write to the file and not read from it.
+
+  // S_IRUSR: This constant specifies the read permission for the owner (user)
+  // of the file. In this case, it allows the owner to read the file.
+
+  // S_IWUSR: This constant specifies the write permission for the owner (user)
+  // of the file. In this case, it allows the owner to write to the file.
 
   if (fd == -1) {
     perror("Unable to touch file");
